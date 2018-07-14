b4128c9d5e59e8c6333c91fe46097fe847cb3575
Unknown <karocksjoelee@gmail.com>
2018-07-14 17:21:15 +0800

    [ Flutter ] AppBar with Text : "EasyList"

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,23 @@
+import 'package:flutter/material.dart';
+
 main() {
-  
+  // runApp(:expects Widget)
+  runApp(MyApp());
+}
+
+// Classes are used to create objects - they serve as blueprint for objects.
+class MyApp extends StatelessWidget {
+  // Flutter will always call the widget's build function
+  build(context) {
+    // when using () means create / construct an object
+    // MaterialApp is a wrapper for our entire app
+    return MaterialApp(
+      // Scaffold creates a new page for the App with nice white background.
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+      ),
+    );
+  }
 }
