b0ad2ebe75188b7c2ae9465907197a239327c7e9
Unknown <karocksjoelee@gmail.com>
2018-07-14 17:51:57 +0800

    [ Flutter ] Adding Cards & Using Assets in App

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('EasyList'),
         ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Text('Food Paradise')
+            ],
+          ),
+        ),
       ),
     );
   }
