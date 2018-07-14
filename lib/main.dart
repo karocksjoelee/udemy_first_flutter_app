80157335701198ad7369be453a5ed71381ec3b18
Unknown <karocksjoelee@gmail.com>
2018-07-14 17:29:29 +0800

    [ Flutter ] Adding Types and fat arrow function

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-main() {
+// ** Shorter version using fat arrow, if have only 1 line of code.
+// void main() => runApp(MyApp());
+
+void main() {
   // runApp(:expects Widget)
   runApp(MyApp());
 }
 
+
+
 // Classes are used to create objects - they serve as blueprint for objects.
 class MyApp extends StatelessWidget {
   // Flutter will always call the widget's build function
-  build(context) {
+  @override
+  build(BuildContext context) {
     // when using () means create / construct an object
     // MaterialApp is a wrapper for our entire app
     return MaterialApp(
