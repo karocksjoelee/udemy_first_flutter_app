ad9d40390cd11659ff96e8299340f42948ea6495
Unknown <karocksjoelee@gmail.com>
2018-07-26 14:39:17 +0800

    [ Flutter ] Performance improved ListView

--- lib/products.dart
+++ lib/products.dart
@@ -5,17 +5,20 @@ class Products extends StatelessWidget {
   final List<String> products;
   Products(this.products);
 
+  Widget _productItemBuilder(BuildContext context , int index) {
+    return Card(
+                child: Column(
+                  children: <Widget>[Image.asset('assets/food.jpg'), Text(products[index])],
+                ),
+              );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ListView(
-      children: products
-          .map((ele) => Card(
-                child: Column(
-                  children: <Widget>[Image.asset('assets/food.jpg'), Text(ele)],
-                ),
-              ))
-          .toList(),
+    return ListView.builder(
+      itemBuilder: _productItemBuilder,
+      itemCount: products.length,
     );
   }
 }
